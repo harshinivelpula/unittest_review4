7c4845be61c0a17822dfb61ff3dedcead67e49c1
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-05 11:55:14 +0530

    addition

---
+++ Democlass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unittestreview4
+{
+    public class Democlass
+    {
+      
+            public int addition(int number1, int number2)
+            {
+                int result = number1 + number2;
+                return result;
+            }
+        
+
+    }
+}
